4fa0c9fb3e647b2349d1a14ba9b2a26c779ad6fa
fluturecode <fluturecode@gmail.com>
2022-03-17 00:55:14 -0400

    configured /hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,18 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
 module.exports = {
-  solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.projectId}`,
+      accounts: []
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.projectId}`,
+      accounts: []
+    }
+  },
+  solidity: '0.8.4',
 };
