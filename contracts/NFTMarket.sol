e47eaffbb9de39c74af6a16e9edd44973fb18d28
Elizabeth Eidelson <fluturecode@gmail.com>
2022-03-25 22:06:58 -0400

    added createMarketSale to NFTMarketplace

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -76,4 +76,23 @@ contract NFTMarket is ReentrancyGuard {
       false
     );
   }
+
+  function createMarketSale(
+    address nftContract,
+    uint256 itemId
+  ) public payable nonReentrant {
+    uint price = idToMarketItem[itemId].price;
+    uint tokenId = idToMarketItem[itemId].tokenId;
+    require(msg.value == price, 'Please submit the asking price in order to complete the purchase.');
+
+    idToMarketItem[itemId].seller.transfer(msg.value);
+    IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);
+    idToMarketItem[itemId].owner = payable(msg.sender);
+    idToMarketItem[itemId].sold = true;
+    _itemSold.increment();
+    payable(owner).transfer(listingPrice);
+  
+  }
+
+  
 }
